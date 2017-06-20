417aaf02b794054f4a4b76abe53af54a317f8c42
Ashok <Ashok@192.168.0.3>
2017-06-20 15:37:30 +0530

    adding more requirements to Teacher

--- src/com/app/Teacher.java
+++ src/com/app/Teacher.java
@@ -5,14 +5,39 @@ public class Teacher
 private int tid;
 private String tname;
 private double tsal;
-private String taddr;
-public Teacher(int tid, String tname, double tsal, String taddr) {
+private boolean flag;
+public Teacher(int tid, String tname, double tsal) {
 	super();
 	this.tid = tid;
 	this.tname = tname;
 	this.tsal = tsal;
-	this.taddr = taddr;
+	
 }
+
+public Teacher(int tid, String tname, double tsal, boolean flag) {
+	super();
+	this.tid = tid;
+	this.tname = tname;
+	this.tsal = tsal;
+	this.flag = flag;
+}
+
+public String getTname() {
+	return tname;
+}
+
+public void setTname(String tname) {
+	this.tname = tname;
+}
+
+public boolean isFlag() {
+	return flag;
+}
+
+public void setFlag(boolean flag) {
+	this.flag = flag;
+}
+
 public int getTid() {
 	return tid;
 }
@@ -25,16 +50,10 @@ public double getTsal() {
 public void setTsal(double tsal) {
 	this.tsal = tsal;
 }
-public String getTaddr() {
-	return taddr;
-}
-public void setTaddr(String taddr) {
-	this.taddr = taddr;
-}
 @Override
 public String toString() {
 	return "Teacher [tid=" + tid + ", tname=" + tname + ", tsal=" + tsal
-			+ ", taddr=" + taddr + "]";
+			+ "]";
 }
 
 }
