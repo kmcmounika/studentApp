c1f8cc66ab70d4471466b2f1b443828821ef91f1
Ashok <Ashok@192.168.0.3>
2017-06-20 15:23:52 +0530

    1st time changes in student file

--- src/com/app/Student.java
+++ src/com/app/Student.java
@@ -4,6 +4,9 @@ public class Student
 {
 	private int sid;
 	private String sname;
+	private double sfee;
+	private int marks;
+	
 	public int getSid() {
 		return sid;
 	}
@@ -13,11 +16,34 @@ public class Student
 	public String getSname() {
 		return sname;
 	}
+	
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", sfee=" + sfee
+				+ ", marks=" + marks + "]";
+	}
 	public void setSname(String sname) {
 		this.sname = sname;
 	}
-	public Student() {
+	
+	public double getSfee() {
+		return sfee;
+	}
+	public void setSfee(double sfee) {
+		this.sfee = sfee;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public Student(int sid, String sname, double sfee, int marks) {
 		super();
+		this.sid = sid;
+		this.sname = sname;
+		this.sfee = sfee;
+		this.marks = marks;
 	}
 	
 
